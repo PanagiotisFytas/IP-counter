83804f5f60ed5089245543c3f38790acd62f1f43
pf2418 <panagiotis.fytas19@imperial.ac.uk>
2020-08-20 15:09:46 +0300

    Enhance test to check that the the count is updated correctly

--- src/test_main.py
+++ src/test_main.py
@@ -3,11 +3,15 @@ from .main import app
 
 client = TestClient(app)
 
-def test_post_log_message():
-    test_request = { "timestamp": "2020-06-24T15:27:00.123456Z", 
+test_request = { "timestamp": "2020-06-24T15:27:00.123456Z", 
                      "ip": "83.150.59.250", 
                      "url": "some/url" 
-                   }
+               }
+    
+ip_to_test = ["83.150.59.250", "83.150.59.251", "83.150.59.250", "83.150.59.250"
+              "83.150.59.252", "83.150.59.251"]
+
+def test_post_log_message():
     response = client.post("/logs", json=test_request)
     assert response.status_code == 200
     assert response.json() == {"message": "IP registered", "ip": "83.150.59.250"}
@@ -16,3 +20,15 @@ def test_get_count():
     response = client.get("/visitors")
     assert response.status_code == 200
     assert response.json() == {"Unique IP addresses": 1}
+
+    for ip in ip_to_test:
+        test_request['ip'] = ip
+        response = client.post("/logs", json=test_request)
+        assert response.status_code == 200
+        assert response.json() == {"message": "IP registered", "ip": ip}
+
+
+    response = client.get("/visitors")
+    assert response.status_code == 200
+    assert response.json() == {"Unique IP addresses": 3}
+
