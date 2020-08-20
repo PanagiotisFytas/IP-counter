2c4a8accdad4683c271fb89d72170046db19852a
pf2418 <panagiotis.fytas19@imperial.ac.uk>
2020-08-20 12:01:58 +0300

    Add unitests for api calls

---
+++ src/test_main.py
@@ -0,0 +1,18 @@
+from fastapi.testclient import TestClient
+from .main import app
+
+client = TestClient(app)
+
+def test_post_log_message():
+    test_request = { "timestamp": "2020-06-24T15:27:00.123456Z", 
+                     "ip": "83.150.59.250", 
+                     "url": "some/url" 
+                   }
+    response = client.post("/logs", json=test_request)
+    assert response.status_code == 201
+    assert response.json() == {"message": "IP registered", "ip": "83.150.59.250"}
+
+def test_get_count():
+    response = client.get("/visitors")
+    assert response.status_code == 200
+    assert response.json() == {"non_unique_count": 0}
