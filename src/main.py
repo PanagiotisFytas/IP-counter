449a6e6a65289016d0022c152c46480ab09b202c
pf2418 <panagiotis.fytas19@imperial.ac.uk>
2020-08-20 14:39:28 +0300

    Add the actual functionality to the GET and POST methods

--- src/main.py
+++ src/main.py
@@ -1,19 +1,37 @@
 from pydantic import BaseModel
 from ipaddress import IPv4Address
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+from datasink import DataSink
+
 
 class Item(BaseModel):
     timestamp: str
-    ip: IPv4Address
+    ip: str
     url: str
 
-app = FastAPI()
 
-@app.post("/logs", status_code=201)
-def post_log_message(item: Item):
-    return {'message': 'IP registered', 'ip': item.ip}
+CACHE_SIZE = 10000
+app = FastAPI()
+app.state.sink = DataSink(max_cache_size=CACHE_SIZE)
+
+
+@app.post("/logs", status_code=200)
+def post_log_message(item: Item, request: Request, response: Response):
+    try:
+        request.app.state.sink.write(item.ip) # write the ip address in the data sink
+        msg = {"message": "IP registered", "ip": item.ip}
+    except Exception as e:
+        msg = {"message": "POST Request failed", "error message": str(e)}
+        response.status_code = 500 # Internal Server Error
+    return msg
 
 
 @app.get("/visitors", status_code=200)
-def get_count():
-    return {"non_unique_count": 0}
+def get_count(request: Request, response: Response):
+    try:
+        cnt = app.state.sink.get_unique_addresses_counter()
+        msg = {"Unique IP addresses": cnt}
+    except Exception as e:
+        msg =  {"message": "GET Request failed", "error message": str(e)}
+        response.status_code = 500 # Internal Server Error
+    return msg
